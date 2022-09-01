b99016686f423474b3e214171b9c01a53fe60bc3
Magdalena Nolde <magdalena@nolde.de>
2022-09-01 10:19:33 +0200

    next updates

--- project/src/components/Column.js
+++ project/src/components/Column.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TasksContext } from "../contexts/TasksContext";
 
 function Column(props) {
-  const { title, items, count } = props;
+  const { moveNext } = useContext(TasksContext);
+  const { movePrev } = useContext(TasksContext);
+  const { title, items, limit, id } = props;
   function renderItems() {
     return items.map((item) => {
       return (
         <div className="column__input">
           <div className="input__author"> Resposible: {item.author}</div>
           <div className="input__taskname">Task: {item.taskName}</div>
+          <button
+            className="input__button--prev"
+            onClick={() => movePrev(id, item)}
+          >
+            Prev
+          </button>
+          <button
+            className="input__button--next"
+            onClick={() => moveNext(id, item)}
+          >
+            Next
+          </button>
         </div>
       );
     });
@@ -15,7 +30,10 @@ function Column(props) {
   return (
     <div className="kanban__column">
       <div className="column__title">{title} </div>
-      <div className="column__limit"> ({count}/5)</div>
+      <div className="column__limit">
+        {" "}
+        ({items.length}/{limit})
+      </div>
       <div className="column__items">{renderItems()}</div>
     </div>
   );
--- project/src/components/Kanban.js
+++ project/src/components/Kanban.js
@@ -12,42 +12,116 @@ function Kanban() {
       title: "Requested",
       items: [],
       count: 0,
+      limit: 1,
     },
     {
       id: 2,
       title: "Backlog",
       items: [],
       count: 0,
+      limit: 2,
     },
     {
       id: 3,
       title: "Working",
       items: [],
       count: 0,
+      limit: 1,
     },
     {
       id: 4,
       title: "Waiting",
       items: [],
       count: 0,
+      limit: 1,
     },
     {
       id: 5,
       title: "Review",
       items: [],
       count: 0,
+      limit: 1,
     },
     {
       id: 6,
       title: "Done",
       items: [],
       count: 0,
+      limit: 1,
     },
   ];
 
   const [state, setState] = useState(initState);
+  // const [firstColumn] = state;
+  // if (firstColumn.limit === firstColumn.items.length) {
+  // alert("You exedeed the limit of tasks");
+  // return null;
+  // }
+
+  const moveNext = (idColumnCurrent, task, currentColumn) => {
+    const currentIndex = state.findIndex((item) => item.id === idColumnCurrent);
+    const nextColumn = state.find(
+      (column) => column.id === idColumnCurrent + 1
+    );
+    console.log(nextColumn);
+    if (nextColumn.limit === nextColumn.items.length) {
+      alert("You exedeed the limit of task in this column");
+      return null;
+    }
+
+    const nextIndex = currentIndex + 1;
+
+    const newState = state.map((column) => {
+      if (column.id === idColumnCurrent) {
+        const newItems = column.items.filter((item) => item.id !== task.id);
+        return { ...column, items: newItems };
+      }
+      return column;
+    });
+
+    const newState2 = newState.map((column, index) => {
+      if (nextIndex === index) {
+        return { ...column, items: [...column.items, task] };
+      }
+      return column;
+    });
+
+    setState(newState2);
+  };
+
+  const movePrev = (idColumnCurrent, task) => {
+    const currentIndex = state.findIndex((item) => item.id === idColumnCurrent);
+    const nextIndex = currentIndex - 1;
+    const prevColumn = state.find(
+      (column) => column.id === idColumnCurrent - 1
+    );
+    console.log(prevColumn);
+    if (prevColumn.limit === prevColumn.items.length) {
+      alert("You exedeed the limit of task in this column");
+      return null;
+    }
+    const newState = state.map((column) => {
+      if (column.id === idColumnCurrent) {
+        const newItems = column.items.filter((item) => item.id !== task.id);
+        return { ...column, items: newItems };
+      }
+      return column;
+    });
+    const newState2 = newState.map((column, index) => {
+      if (nextIndex === index) {
+        return { ...column, items: [...column.items, task] };
+      }
+      return column;
+    });
+    setState(newState2);
+  };
 
   const addTask = (author, taskName) => {
+    const [firstColumn] = state;
+    if (firstColumn.limit === firstColumn.items.length) {
+      alert("You exedeed the limit of task in this column");
+      return null;
+    }
     const newState = state.map((column, index) => {
       if (index === 0) {
         return {
@@ -56,12 +130,6 @@ function Kanban() {
             ...column.items,
             { author: author, taskName: taskName, id: uuid() },
           ],
-        };
-      }
-      if (column.items.length < 6) {
-        return {
-          ...column,
-          count: column.items.length,
         };
       }
       return column;
@@ -75,7 +143,8 @@ function Kanban() {
         <Column
           title={column.title}
           items={column.items}
-          count={column.count}
+          limit={column.limit}
+          id={column.id}
         />
       );
     });
@@ -87,6 +156,8 @@ function Kanban() {
         value={{
           state,
           addTask,
+          moveNext,
+          movePrev,
         }}
       >
         <div className="kanban__title">KANBAN BOARD</div>
